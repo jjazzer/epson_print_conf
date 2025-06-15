42e8998cf3d6af565cfa8fbfbb0e0c4447c8b15b
jjazzer <jhonjassergarcia@gmail.com>
2025-06-15 16:50:11 +0800

    Ready

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ source "$SCRIPT_DIR/bin/activate"
 python3 -m venv "$SCRIPT_DIR"
 cd "$SCRIPT_DIR"
 "$SCRIPT_DIR/bin/pip" install -r requirements.txt
+cp "$SCRIPT_DIR/.gitignore.bak" "$SCRIPT_DIR/.gitignore"
+for script in $SCRIPT_DIR/bin/epc*; do
+  sln $script ~/.local/bin/$(basename $script)
+done
